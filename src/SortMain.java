e41fe2f2721a9d8eec255a2898dd2d76132ea239
Skandina <skandinapark@gmail.com>
2021-11-18 19:52:48 +0100

    Main program

---
+++ src/SortMain.java
@@ -0,0 +1,13 @@
+public class SortMain {
+    public static void main(String[] args){
+
+        int[] test = {1,0,-7,5,-3, 5, 2, -10};
+        int[] Final = Sorter.sort(test);
+
+        Sorter.PrintSort(Final);
+        
+        
+
+
+    }
+}        
