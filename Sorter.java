bba62fc698f1ddc810ecfb8f203ca632344e32f9
Skandina <skandinapark@gmail.com>
2021-11-19 21:18:41 +0100

    last editing, hopefully

---
+++ Sorter.java
@@ -0,0 +1,23 @@
+public class Sorter {
+
+    public static int[] sort(int[] unsorted) {
+        for(int i = 0; i < unsorted.length; i++) {
+            for(int j=i+1; j<unsorted.length; j++) {
+                if(unsorted[i] > unsorted[j]) {
+                    int temp = unsorted[i]; 
+                    unsorted[i]=unsorted[j]; 
+                    unsorted[j]=temp;
+                }
+            }
+        }
+
+         return unsorted; 
+    }
+    
+    public static void PrintSort(int[] unsorted){
+        for (int i = 0; i < unsorted.length; i++) {
+            System.out.print(unsorted[i]+" ");
+        }
+        System.out.println();
+    }
+}
